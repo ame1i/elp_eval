77fd279ffebc377dd2a4a1d80eb7f019f7663234
ame1i <91328757+ame1i@users.noreply.github.com>
2022-01-25 21:37:17 +0100

    js

---
+++ q1_.js
@@ -0,0 +1,7 @@
+//consigne : "a ba a".replace("a", "z")
+/*
+Le modificateur I est employé pour effectuer la correspondance ne distinguant pas majuscules et minuscules.
+Le modificateur g est employé pour effectuer une correspondance globale (trouvez toutes les correspondances plutôt qu'en s'arrêtant après la première trouvée).*/
+
+let ex = "a ba a"/"a"/"z";
+console.log(ex);
---
+++ q2.js
@@ -0,0 +1,4 @@
+a = "Bonjour M. stephane.\n Je m\'appelle Stephane. Super Stephane";
+re = /Stephane/George/g/;
+k = a.replace(re, "George");
+console.log(k);
